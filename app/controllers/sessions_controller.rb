8469d595035c42c19c04febf0bf358e4aec0711b
ken <ken.demarest@gmail.com>
2012-10-01 00:21:35 -0500

    Now shows env vars

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,14 @@ class SessionsController < ApplicationController
 	  if session[:user_id]
 	    # Means our user is signed in. Add the authorization to the user
 	    User.find(session[:user_id]).add_provider(auth_hash)
-	    render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+
+	    s = "Successful login via #{auth_hash["provider"].capitalize}<br />"
+	    s += "env vars:<br />"
+	    ENV.each_pair do |k,v|
+			s += "env['"+k+"'] = '"+v+"'<br />"
+		end
+
+	    render :text => s
 	  else
 	    # Log him in or sign him up
 	    auth = Authorization.find_or_create(auth_hash)
